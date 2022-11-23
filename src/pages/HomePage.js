3a7e2cd3d04b827333a2636ecf151cb56c3ed828
Silviagstnn <104710527+Silviagstnn@users.noreply.github.com>
2022-11-23 20:58:27 +0700

    Add data statistik
    Add data statistik menggunakan api https://apicovid19indonesia-v2.vercel.app/api/indonesia.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,33 @@
-import '../css/homepage.css'
+import { useEffect, useState } from "react"
 
 function HomePage(){
+    const [data, setData] = useState([])
+
+    const getDataUsers = async () => {
+        const response = await fetch('https://apicovid19indonesia-v2.vercel.app/api/indonesia')
+        const data = await response.json()
+        console.log(data)
+        setData(data);
+    }
+
+    useEffect(() => {
+        getDataUsers()
+    }, [])
+
     return (
-       <div>
-            <div className="homepage">
-                <p>uguadadfyg</p>
+        <div className="App">
+            <h3>Statistik Covid-19 Di Indonesia</h3>
+            <div>
+                <ul>
+                    <li>Meninggal : {data.meninggal}</li>
+                    <li>Kasus Terkonfirmasi : {data.positif}</li>
+                    <li>Sedang Dirawat : {data.dirawat}</li>
+                    <li>Berhasil Sembuh :{data.sembuh}</li>
+                </ul>
             </div>
-       </div>
+        
+        </div>
     )
 }
 
-export default HomePage;
+export default HomePage
